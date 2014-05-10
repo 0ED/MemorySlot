afa1ca4949634fff394195dda43e5bbfd45d1233
TasukuTakahashi <tasukutakahashi@MacBook-Pro-for-TasukuTakahashi.local>
2014-05-10 23:02:04 +0900

    init

--- rpc/rmi/LaasServerInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-
-public interface LaasServerInterface extends Remote
-{
-	int getTotalRunningYardage(String aStr) throws RemoteException;
-	int getTotalPassingYardage(String aStr) throws RemoteException;
-	int getTotalTurnovers(String aStr) throws RemoteException;
-}
